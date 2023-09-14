722cfb099af31f7f3d3ec8fab6cfa4cadd6783e9
lolhook <144627983+lolhook@users.noreply.github.com>
2023-09-14 07:52:45 +0100

    Add files via upload

--- status.js
+++ status.js
@@ -1,9 +1,34 @@
-var status_url = "https://raw.githubusercontent.com/lolhook/lolhook.xyz/main/status.json";
+var status_url = "https://raw.githubusercontent.com/lolhook/lolhook.github.io/main/status.json";
 var status_emoji = {0:"🟢", 1:"🟡", 2:"🔴", 3:"🟠",4:"⚪"};
+
+
 fetch(status_url).then(res => res.json().then(result => {
     console.log(result);
     for (var game in result) {
-        var object = document.getElementById(game);
-        object.innerHTML += (" " + status_emoji[result[game]]);
+        console.log(game, result[game]);
+
+        var title = document.createElement("p");
+        title.innerHTML = game + " " + status_emoji[result[game].status];
+        
+        var br = document.createElement("br");
+
+        var link = document.createElement("a");
+        link.href = result[game].link;
+        link.className = "game-link";
+        link.innerHTML = result[game].link;
+
+        var div = document.createElement("div");
+        div.id = "seperate";
+
+        var div2 = document.createElement("div");
+        div2.id = "divider";
+
+        var main = document.getElementById("main")
+
+        main.appendChild(title);
+        main.appendChild(br);
+        main.appendChild(link);
+        main.appendChild(div);
+        main.appendChild(div2);
     };
 }));
